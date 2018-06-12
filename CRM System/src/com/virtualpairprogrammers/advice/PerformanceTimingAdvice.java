1eae6ab489f293955d3e6c64a7df4402ff218082
anderson <anderson@java-devops>
2018-06-12 08:53:07 -0400

    verified and refactored the latest code in the CRM System
    Matched the code in the videos

---
+++ CRM System/src/com/virtualpairprogrammers/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,25 @@
+package com.virtualpairprogrammers.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformanceTimingAdvice
+{
+	private static final int NANOSECONDS_IN_A_MILLISECOND = 1000000;
+
+	public Object recordTiming(ProceedingJoinPoint jp) throws Throwable
+	{
+		double timeNow = System.nanoTime();
+		
+		try
+		{
+			Object returnValue = jp.proceed();
+			return returnValue;
+		}
+		finally
+		{
+			double timeAfter = System.nanoTime();
+			double timeTaken = timeAfter - timeNow;
+			System.out.println("The method " + jp.getSignature().getName() + " took " + timeTaken / NANOSECONDS_IN_A_MILLISECOND + " miliseconds");
+		}
+	}
+}
